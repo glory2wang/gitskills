0a99897374dba17c02047edd8124dc7a2d18acc2
wangzhen_369@163.com <wangzhen_369@163.com>
2018-07-11 14:57:54 +0800

    node节点

---
+++ Sensitive/src/com/er/cha/shu/TreeNode.java
@@ -0,0 +1,15 @@
+package com.er.cha.shu;
+
+/**
+ * Created by wz on 2018/7/11.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
